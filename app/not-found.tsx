bdc3655966ca54d944d127f2852ee99e7774c5b9
Elbi <rhafaelbijaksana04@gmail.com>
2025-09-14 22:25:16 +0700

    feat: membuat halaman not found

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Not Found | Rhafael Bijaksana",
+};
+
+
+export default function NotFoundPage () {
+    return (
+        <div className="flex flex-col gap-2">
+            <h1 className="text-4xl">Sorry, </h1>
+            <p className="text-xl">the page you are looking for was not found.</p>
+        </div>
+    )
+}
