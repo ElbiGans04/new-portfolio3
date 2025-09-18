c6b947d14fbc29bb299bab8390ef150549923674
Elbi <rhafaelbijaksana04@gmail.com>
2025-09-18 22:03:26 +0700

    feat: membuat halaman portfolio detail

---
+++ app/portfolios/[id]/page.tsx
@@ -0,0 +1,45 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolios | Rhafael Bijaksana",
+};
+
+export default function PortfoliosPage() {
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <h1 className="text-5xl">CGS: Itradefund</h1>
+        <div className="flex flex-col gap-1">
+          <p>
+            <span className="font-bold">Development Time</span> : 20 Aug 2025 -
+            31 Mai 2021
+          </p>
+          <p>
+            <span className="font-bold">Technology Used</span> : Next Js, React
+            Js, Flutter
+          </p>
+          <p>
+            <span className="font-bold">Url Project</span> : 20 Aug 2025 - 31
+            Mai 2021
+          </p>
+        </div>
+      </div>
+      <div className="mt-8">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          dolore aliquam possimus nisi praesentium saepe numquam iste libero
+          accusantium id eius vel soluta, dolor deleniti eum aspernatur autem
+          molestias amet non doloremque. Quod voluptatibus nesciunt totam
+          quaerat veritatis inventore quae corporis porro similique voluptas qui
+          atque aspernatur perferendis impedit, pariatur rerum autem, est
+          possimus ipsa debitis obcaecati nihil illum? Rerum delectus sit fugiat
+          laboriosam perferendis aperiam, officia rem, perspiciatis, neque ipsum
+          iste. Voluptate fuga odit rem nihil, error praesentium architecto
+          mollitia laudantium at dolorum eligendi assumenda sapiente repudiandae
+          minima ratione sequi totam reiciendis impedit quasi nostrum dicta
+          consectetur esse commodi?
+        </p>
+      </div>
+    </>
+  );
+}
