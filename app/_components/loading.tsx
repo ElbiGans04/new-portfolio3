2e0e612a49c08419e63ba591319f6ca0f35a279e
Elbi <rhafaelbijaksana04@gmail.com>
2025-09-29 19:58:39 +0700

    feat: menambahkan loading.js

---
+++ app/_components/loading.tsx
@@ -0,0 +1,9 @@
+export default function LoadingComponents() {
+  return (
+    <div className="flex gap-5 items-center">
+      <div className="bg-[#0a0a0a] dark:bg-white text-[#ededed] dark:text-[#171717] animate-pulse p-4 rounded">
+        <p className="font-bold">Loading...</p>
+      </div>
+    </div>
+  );
+}
